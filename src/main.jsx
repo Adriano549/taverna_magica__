e946e9b613b393d39f6519739a46fbdbd8f4f089
Adriano549 <165341189+Adriano549@users.noreply.github.com>
2024-07-22 10:44:10 -0300

    config

--- src/main.jsx
+++ src/main.jsx
@@ -1,72 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import HomePage from './pages/homePage.jsx'
-import Sobre from './components/Sobre/sobre.jsx'
-import ProdutosPage from './pages/ProdutosPage.jsx'
-import ProductPurchase from './components/produtoCompra/index.jsx'
-import LoginPage from './pages/loginPage.jsx'
-import ForgetpasswordPage from './pages/passowrdForget.jsx'
-import CreatContPage from './pages/creatContPage.jsx'
-import PageCarrinho from './pages/compraPage.jsx'
-import PagamentoPage from './pages/pagamentoPage.jsx'
-import PagamentoComfirmadoPage from './pages/pagamentoComfirmadoPage.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/homePage.jsx';
+import Sobre from './components/Sobre/sobre.jsx';
+import ProdutosPage from './pages/ProdutosPage.jsx';
+import ProductPurchase from './components/produtoCompra/index.jsx';
+import LoginPage from './pages/loginPage.jsx';
+import ForgetpasswordPage from './pages/passowrdForget.jsx';
+import CreatContPage from './pages/creatContPage.jsx';
+import PageCarrinho from './pages/compraPage.jsx';
+import PagamentoPage from './pages/pagamentoPage.jsx';
+import PagamentoComfirmadoPage from './pages/pagamentoComfirmadoPage.jsx';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-          path: "/",
-          element: <HomePage />,
-        },
-        {
-          path: "/sobre",
-          element: <Sobre />,
-        },
-        {
-          path: "/produtos",
-          element: <ProdutosPage />,
-        },
-        {
-          path: "/produtos/:id",
-          element: <ProductPurchase />
-        },
-        {
-          path: "/login",
-          element: <LoginPage />
-        },
-        {
-          path: "/loginSenha",
-          element: <ForgetpasswordPage />
-        },
-        {
-          path: "/criar-conta",
-          element: <CreatContPage />
-        },
-        {
-          path: "/carrinho",
-          element: <PageCarrinho />
-        },
-        {
-          path:"/pagamento",
-          element:<PagamentoPage/>
-        },
-        {
-          path:"/pagamento-confirmado",
-          element: <PagamentoComfirmadoPage/>
-        }
-      ]
-    }
-  ])
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/sobre" element={<Sobre />} />
+      <Route path="/produtos" element={<ProdutosPage />} />
+      <Route path="/produtos/:id" element={<ProductPurchase />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/loginSenha" element={<ForgetpasswordPage />} />
+      <Route path="/criar-conta" element={<CreatContPage />} />
+      <Route path="/carrinho" element={<PageCarrinho />} />
+      <Route path="/pagamento" element={<PagamentoPage />} />
+      <Route path="/pagamento-confirmado" element={<PagamentoComfirmadoPage />} />
+    </Route>
+  </Routes>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <AppRoutes />
+    </HashRouter>
   </React.StrictMode>,
-)
-
+);
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"taverna_magica__"
+  base:"/taverna_magica__/"
 })
